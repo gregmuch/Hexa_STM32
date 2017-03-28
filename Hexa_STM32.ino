10ff389b45e9f1439ce3118a6ef39cc163691cc7
Gregoire JOUAN <gregoire.jouan@gmail.com>
2017-03-28 21:29:52 +0200

    on going version

--- Hexa_STM32.ino
+++ Hexa_STM32.ino
@@ -1,19 +1,19 @@
 #include "hexaServo.h"
-#include "PS2X_lib.h"
+//#include "PS2X_lib.h"
+#include <PS2X_lib.h>
 
 
-// Define pins used to connect the PS2 controller
-#define PS2_DAT     14    
-#define PS2_CMD     16
-#define PS2_SEL     7
-#define PS2_CLK     15
-
-// SDA pin = D2 or pin 2 on a spark pro micro
-// SCL pin = D3 or pin 3 on a spark pro micro
+int SPI_freq = 500000; 
+SPIClass spi(2);
+PS2X ps2x(spi, SPI_freq); // create PS2 Controller Class
 
 
-// int g_hexaAngle;
-// int g_hexaSpeed;
+
+//right now, the library does NOT support hot pluggable controllers, meaning 
+//you must always either restart your Arduino after you conect the controller, 
+//or call config_gamepad(pins) again after connecting the controller.
+byte vibrate = 0;
+
 
 int error = 0; // returned status of PS2 controller connection.
 // error = 0 -> everything fine, controller found and well configured
@@ -33,9 +33,6 @@ double stepSize = 0.0;
 int height = 0;
 double angle =0;
 
-PS2X ps2x;        // create PS2 Controller Class
-
-
 
 void setup(){
 
@@ -44,8 +41,8 @@ void setup(){
   Serial.println();
   Serial.println( "Serial connexion established" );
 
-  error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, false, false);
-
+   error = ps2x.config_gamepad(31);   //setup GamePad(clock, command, attention, data) pins, check for error
+ 
   if (error == 1){
     Serial.println();
     Serial.println( "No PS2 gamepad found" );
